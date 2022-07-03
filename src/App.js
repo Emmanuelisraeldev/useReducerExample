22743cd5b2b49ef33ad69afc51b3cc64b8323337
Emmanuelisraeldev <67147427+Emmanuelisraeldev@users.noreply.github.com>
2022-07-03 10:55:18 +0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,85 @@
+import "./styles.css";
+import React, { useReducer } from "react";
+import Modal from "./Modal";
+
+const data = [
+  { id: 1, name: "john" },
+  { id: 2, name: "peter" },
+  { id: 3, name: "susan" },
+  { id: 4, name: "anna" }
+];
+
+const App = () => {
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case "name":
+        return { ...state, name: action.payload };
+      case "newitem":
+        const newPeople = [...state.people, action.payload];
+        return {
+          ...state,
+          people: newPeople,
+          showModal: true,
+          modalContent: "item added"
+        };
+      case "setname":
+        return { ...state, name: action.payload };
+      case "novalue":
+        return {
+          ...state,
+          showModal: true,
+          modalContent: "enter value"
+        };
+      case "closemodal":
+        return { ...state, showModal: false };
+      default:
+        throw new Error(`Unknown action type: ${action.type}`);
+    }
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (state.name) {
+      const newItem = { id: new Date().getTime().toString(), name: state.name };
+      dispatch({ type: "newitem", payload: newItem });
+    } else {
+      dispatch({ type: "novalue" });
+    }
+    dispatch({ type: "setname", payload: "" });
+  };
+
+  const closeModal = () => {
+    dispatch({ type: "closemodal" });
+  };
+  const [state, dispatch] = useReducer(reducer, {
+    name: "",
+    people: data,
+    modalContent: "",
+    showModal: false
+  });
+  console.log(state);
+
+  return (
+    <>
+      {state.showModal && (
+        <Modal modalContent={state.modalContent} closeModal={closeModal} />
+      )}
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={state.name}
+          onChange={(e) => dispatch({ type: "name", payload: e.target.value })}
+        />
+        <button type="submit">add</button>
+      </form>
+      {state.people.map((person) => {
+        return (
+          <div key={person.id}>
+            <h3>{person.name}</h3>
+          </div>
+        );
+      })}
+    </>
+  );
+};
+
+export default App;
